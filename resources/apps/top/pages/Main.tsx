713a9355105e55c54a88dca677120eab8ecaa04a
ray0495 <ramudalambda901@gmail.com>
2024-11-27 20:52:05 +0900

    Build top-page

--- resources/apps/top/pages/Main.tsx
+++ resources/apps/top/pages/Main.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { route, Route } from "ziggy-js";
 import { useAuthContext } from "../../../hooks/AuthProvider";
-import { Link } from "@inertiajs/react";
+import Board from "../components/Templates/Board";
+import Information from "../components/Templates/Information";
+import ArticleList from "../components/Templates/ArticleList";
+import AppFooter from "../components/Templates/AppFooter";
 
 const Main: React.FC = () => {
     const { authStatus, login, logout } = useAuthContext();
 
     return (
-        <div>
-            <h1>トップページやで</h1>
-        </div>
+        <>
+        <Board/>
+        <Information/>
+        <ArticleList label="Topics" link={ route("/") }/>
+        <ArticleList label="Gourmet" link={ route("/") }/>
+        <AppFooter/>
+        </>        
     );
 };
 
