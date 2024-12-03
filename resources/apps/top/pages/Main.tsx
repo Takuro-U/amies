ab2936c6b298cb90ea59065905e2588274c9cf7d
ray0495 <ramudalambda901@gmail.com>
2024-12-03 20:09:14 +0900

    Fixed app layout

--- resources/apps/top/pages/Main.tsx
+++ resources/apps/top/pages/Main.tsx
@@ -7,19 +7,21 @@ import ArticleList from "../components/Templates/ArticleList";
 import AppFooter from "../components/Templates/AppFooter";
 import BackGround from "../components/Templates/Background";
 
+import styles from "../styles/top.module.scss"
+import classNames from "classnames";
 const Main: React.FC = () => {
     const { authStatus, login, logout } = useAuthContext();
 
     return (
         <>
         {/* 背景より下にめり込まないようにするためにz-10を設定（背景はz-0） */}
-        <div className="relative z-10">
+        <div className={classNames("relative z-10", styles.wrapper )}>
             <Board/>
             <Information/>
             <ArticleList label="Topics" link={ route("/") }/>
             <ArticleList label="Gourmet" link={ route("/") }/>
-            <AppFooter/>
         </div>
+        <AppFooter/>
         <BackGround/>
         </>        
     );
