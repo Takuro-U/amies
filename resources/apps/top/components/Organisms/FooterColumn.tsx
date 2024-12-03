5abf86c1174e4a190786b999a1fac943ab67a242
ray0495 <ramudalambda901@gmail.com>
2024-12-03 20:08:39 +0900

    Fixed Footer style

--- resources/apps/top/components/Organisms/FooterColumn.tsx
+++ resources/apps/top/components/Organisms/FooterColumn.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { FooterColumnType } from "../../../../types/top";
 
+import styles from "../../styles/footer.module.scss";
+
 export const FooterColumn: FC<FooterColumnType> = ({heading, node})=>{
     return (
-        <section className="footerCol">
-            <h2 className="colHeading px-3">{ heading }</h2>
-            <hr className="colLine m-1 border-slate-600"/>
-            <div className="colBody px-3">
+        <section className={ styles.column }>
+            <h2 className="px-3">{ heading }</h2>
+            <hr className="m-1 border-slate-600"/>
+            <div className="px-3">
             { node }
             </div>
         </section>
--- resources/apps/top/components/Templates/AppFooter.tsx
+++ resources/apps/top/components/Templates/AppFooter.tsx
@@ -4,11 +4,12 @@ import styles from "../../styles/footer.module.scss";
 
 // SAMPLE DATA
 import { footerColumn } from '../../ts/samples';
+import classNames from 'classnames';
 export default function AppFooter(){
     return (
-        <footer className="w-full bg-neutral-200 p-2">
+        <footer className={classNames("w-full bg-neutral-200 p-2", "relative z-10")}>
             <h1 className="text-xl">AMie's</h1>
-            <div className="footerContainer">
+            <div className={styles.container}>
             { footerColumn.map((col, key)=> <FooterColumn key={ key } heading={ col.heading } node={ col.node }/>)}
             </div>
         </footer>
