c844f9f46589d4054cb82921ca5ecf9445bcbfd2
ray0495 <ramudalambda901@gmail.com>
2024-12-03 17:53:31 +0900

    Fixed indent

--- resources/apps/top/components/Organisms/InformationLink.tsx
+++ resources/apps/top/components/Organisms/InformationLink.tsx
@@ -5,7 +5,9 @@ import { InformationType } from "../../../../types/top";
 export const InformationLink: FC<InformationType> = ( infomation: InformationType )=>{
     return (
         <article className="flex justify-between my-1 py-1 mx-3">
-            <h3 className="underline underline-offset-2  text-base">{ infomation.date.join("-") + " :" + infomation.title }</h3>
+            <h3 className="underline underline-offset-2  text-base">
+                { infomation.date.join("-") + " :" + infomation.title }
+            </h3>
         </article>
     )
 }
