13997267849de6549a90b48739481f8a78181ab7
ray0495 <ramudalambda901@gmail.com>
2024-12-03 16:57:35 +0900

    Update motions

--- resources/apps/top/components/Organisms/SnapshotContainer.tsx
+++ resources/apps/top/components/Organisms/SnapshotContainer.tsx
@@ -10,24 +10,23 @@ export const SnapshotContainer: FC<Props> = ( props )=>{
     let random 
     do{
         random = Math.round(Math.random()*100);
-    }while(!(5 < random && random < 45));
+    }while(!(1 <= random && random <= 40));
 
-    console.log(random);
     return (
         <motion.div
             initial={{
                 opacity: 0,
-                x: random < 25 ? "-80vw" : "180vw",
+                x: random < 23 ? "-80vw" : "180vw",
                 y: Math.sin(props.delta)*100+"vh",
             }}
             animate={{
                 opacity: 1,
-                x: random + "vw",
+                x: Math.min(random,40) + "vw",
                 y: (30+random/10)*props.delta + "vw",
             }}
             transition={{
                 duration: 3.0 / 1.4,
-                delay: 3.0/4 * props.delta * 0.6,
+                delay: 2.6/4 * props.delta * 0.8,
                 ease: "easeOut",
             }}>
             <div className="absolute w-[60vw] h-[80vw] flex z-0" style={{clipPath: "polygon(0% 15%, 100% 0%, 100% 85%, 0% 100%)"}}>
