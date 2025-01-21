a57c628aa50a7f7c3361d4f3bcca21bd9db3e7de
ray0495 <ramudalambda901@gmail.com>
2025-01-21 18:30:05 +0900

    Update logic with window.scrollTo and translate

--- resources/common/components/Organism/BackToPageTopButton.tsx
+++ resources/common/components/Organism/BackToPageTopButton.tsx
@@ -7,18 +7,21 @@ export const BackToPageTopButton: FC =()=>{
     onscroll = ()=>{
         if(buttonRef.current){
             let button: HTMLDivElement = buttonRef.current;
-            button.style.bottom = document.getElementsByTagName("html")[0].scrollTop?"1.5rem":"-3.5rem";
+            button.style.translate = document.getElementsByTagName("html")[0].scrollTop?"0 -5rem":"0 5rem";
         }
     }
 
     return (
-        <a href="#"><div
+        <div
         ref={ buttonRef }
         style={{
-            transition: "bottom 0.6s ease-out 0s",
+            transition: "translate 0.5s ease-out",
+        }}
+        onClick={()=>{
+            window.scrollTo({top: 0, left: 0, behavior: "smooth"});
         }}
         className={classNames(
-            "fixed bottom-[-3.5rem] right-6",
+            "fixed bottom-[-3rem] right-2",
             "w-12 h-12 rounded-full",
             "bg-slate-500 opacity-60",
             "z-20",
@@ -26,6 +29,6 @@ export const BackToPageTopButton: FC =()=>{
             "text-white text-lg",
             "cursor-pointer",
         )}
-        >▲</div></a>
+        >▲</div>
     )
 }
