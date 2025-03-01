60174c73db15dc4a2ccdf286c44633fd56fae981
Takuro-U <422314@m.mie-u.ac.jp>
2025-03-01 17:36:59 +0900

    2025-03-01

--- routes/web/console.php
+++ routes/web/console.php
@@ -3,14 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Helpers\InertiaHelper;
+use App\Http\Controllers\CustomAuth\CreateUserController;
 
 Route::prefix('admin')->middleware(['auth', 'can:admin'])->group(function () {
     Route::get('/', function () {
         return InertiaHelper::renderPage('admin', 'main');
     })->name('/console/admin');
+
     Route::get('/user-creator', function () {
         return InertiaHelper::renderPage('admin', 'user_creator');
     })->name('/console/admin/user-creator');
+
+    Route::post('/user-creator', [CreateUserController::class, 'store']);
 });
 
 Route::prefix('restaurant')->middleware(['auth', 'can:restaurant'])->group(function () {
