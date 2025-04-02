fd4ad1f8375717d4ff051cbbdc0ff1fd67eaa8d4
Takuro-U <422314@m.mie-u.ac.jp>
2025-04-02 12:50:38 +0900

    temp

--- resources/console.tsx
+++ resources/console.tsx
@@ -11,7 +11,7 @@ import { consoleRouter } from "./common/ts/router";
 //components
 import AuthProvider from "./hooks/AuthProvider";
 import ModalProvider from "./hooks/ModalProvider";
-import AppShell from "./common/layouts/AppShell";
+import AppShell from "@/common/layouts/AppShell";
 
 createInertiaApp({
     resolve: (key) => {
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,11 @@ export default defineConfig({
             },
         },
     },
+    resolve: {
+        alias: {
+            "@": "/resources",
+        },
+    },
     //
     server: {
         proxy: {
